a94c19f59d0623d898697a20207f392cc65a392e
Burak <75482610+burakcantokses@users.noreply.github.com>
2021-10-29 14:03:46 +0300

    finished in 1.5 hour

---
+++ project3/src/Main.java
@@ -0,0 +1,52 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class Main {
+    public static List<Integer> divideList = new ArrayList<>();
+
+    public static void main(String[] args) {
+        int divide=2, counter = 0;
+        long number = 600851475143L;
+        while (true){
+            System.out.println("Number: " + number);
+            System.out.println("Divide: " + divide);
+            if (number%divide==0) {
+                number = number/divide;
+                if (!divideList.contains(divide)) {
+                    divideList.add(divide);
+                }
+            }else if (number%divide != 0) {
+                divide += 1;
+                continue;
+            }
+            if (number == 1) {
+                break;
+            }
+            counter++;
+        }
+        System.out.println("______________________");
+        for (int i = 0; i < divideList.size(); i++) {
+            System.out.println(divideList.get(i));
+        }
+
+    }
+
+    public static boolean primeCheck(int number) {
+        boolean check = true;
+        for (int i = number-1; i >= 2; i--) {
+            if (number==1) {
+                check = false;
+                break;
+            }
+            if (number==2) {
+                check = true;
+                break;
+            }
+            if (number%i == 0) {
+                check = false;
+                break;
+            }
+        }
+        return check;
+    }
+}
